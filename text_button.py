e5a131d28b28c496d48042d7266e35369ad52ea0
Mirror83 <glenochieng045@gmail.com>
2023-11-09 10:30:40 +0300

    Copied over TextButton from stack_visualization with some modifications

---
+++ text_button.py
@@ -0,0 +1,43 @@
+from typing import Callable, Literal
+
+import pygame as pg
+from pygame import Vector2, Rect, Cursor, SYSTEM_CURSOR_ARROW, SYSTEM_CURSOR_HAND, Surface
+from pygame.font import Font
+
+
+class TextButton:
+    def __init__(self, text: str, top_left: Vector2, on_click_handler: Callable[[], None], font_size: int):
+        self.text = text
+        self.font = Font(None, font_size)
+        self.color: Literal["Black", "Purple", "Red"] = "Black"
+        self.font_surface = self.font.render(self.text, True, self.color).convert_alpha()
+        self.rect = Rect(top_left, self.font_surface.get_size())
+        self.on_click_handler = on_click_handler
+        self.cursors = [Cursor(SYSTEM_CURSOR_ARROW), Cursor(SYSTEM_CURSOR_HAND)]
+        self.cursor = self.cursors[0]
+
+    def render(self, surface: Surface):
+        self.check_hover()
+        self.font_surface = self.font.render(self.text, True, self.color).convert_alpha()
+        surface.blit(self.font_surface, self.rect)
+
+    def is_hover(self):
+        mouse_pos = Vector2(pg.mouse.get_pos())
+        # mouse_pos.x -= Menu.MOUSE_OFFSET  # To account for positioning relative to menu Surface
+        is_hover = self.rect.collidepoint(mouse_pos)
+        return is_hover
+
+    def check_hover(self):
+        if self.is_hover():
+            self.color = "Purple"
+            if self.cursor != self.cursors[1]:
+                self.cursor = self.cursors[1]
+                pg.mouse.set_cursor(self.cursor)
+        else:
+            self.color = "Black"
+            if self.cursor != self.cursors[0]:
+                self.cursor = self.cursors[0]
+                pg.mouse.set_cursor(self.cursor)
+
+    def click(self):
+        self.on_click_handler()
