9585b4d0bdbe86797193923a53cc79d3f121cd52
Mirror83 <glenochieng045@gmail.com>
2023-11-09 10:32:56 +0300

    Overhauled the TextInput class

--- text_input.py
+++ text_input.py
@@ -5,20 +5,31 @@ from pygame import Surface, Vector2, Rect
 
 
 class TextInput:
-    def __init__(self, on_enter: Callable[..., None] = lambda: print("Enter pressed")):
-        self.size = Vector2(140, 35)
+    def __init__(
+            self,
+            top_left: Vector2,
+            size: Vector2 = Vector2(140, 35),
+            font_size: int = 50,
+            on_enter: Callable[..., None] = lambda: print("Enter pressed"),
+            label_text=""
+    ):
+        self.size = size
 
         self.text = ""
+        self.label_text = label_text
+
         self._on_enter = on_enter
 
-        self.has_focus = False
-        self.input_rectangle = Rect(Vector2(0, 0), self.size)
+        self._has_focus = False
+        self.input_rectangle = Rect(top_left, self.size)
 
-        self.font = pg.font.Font(None, 50)
+        self.font = pg.font.Font(None, font_size)
         self._input_color_passive = "Grey"
         self._input_color_active = "Blue"
         self.input_color = self._input_color_passive
         self.text_color = "Black"
+        self.font_surface = self.font.render(self.text, True, self.text_color)
+        self.font_rectangle = self.font_surface.get_rect()
 
     def listen_for_focus(self):
         mouse_pos = pg.mouse.get_pos()
@@ -26,44 +37,38 @@ class TextInput:
 
         if mouse_pressed[0] is True:
             if self.input_rectangle.collidepoint(mouse_pos):
-                self.has_focus = True
+                self._has_focus = True
                 self.input_color = self._input_color_active
             else:
-                self.has_focus = False
+                self._has_focus = False
                 self.input_color = self._input_color_passive
 
-    def listen_for_text(self):
-        self.listen_for_focus()
-        if self.has_focus:
-            event = pg.event.wait()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                elif event.key == pg.K_RETURN:
-                    self.text = ""
-                    self._on_enter()
-                else:
-                    if event.unicode.isprintable():
-                        self.text += event.unicode
+    def has_focus(self):
+        return self._has_focus
 
-    def update_text(self, screen: Surface):
-        font_surface = self.font.render(self.text, True, self.text_color).convert_alpha()
-        font_rectangle = font_surface.get_rect()
-
-        self.input_rectangle = Rect(Vector2(0, 0), self.size)
-        self.input_rectangle.center = screen.get_rect().center
-
-        if font_rectangle.width > self.input_rectangle.width:
-            self.input_rectangle.width = font_rectangle.width + 10
+    def update_text(self, event_key: int, text: str):
+        if event_key == pg.K_BACKSPACE:
+            self.text = self.text[:-1]
+            self.update_text_box_size()
+        elif event_key == pg.K_RETURN:
+            self.text = ""
+            self._on_enter()
+            self.update_text_box_size()
         else:
-            self.input_rectangle.width = self.size.x
+            if text.isprintable():
+                self.text += text
+                self.update_text_box_size()
 
-        self.input_rectangle.center = screen.get_rect().center
-        font_rectangle.center = self.input_rectangle.center
+    def update_text_box_size(self):
+        self.font_surface = self.font.render(self.text, True, self.text_color).convert_alpha()
+        self.font_rectangle = self.font_surface.get_rect()
 
-        screen.blit(font_surface, font_rectangle)
-        pg.draw.rect(screen, self.input_color, self.input_rectangle, 2)
+        self.input_rectangle.width = max(self.size.x, self.font_rectangle.width + 20)
+        self.font_rectangle.midleft = self.input_rectangle.midleft
+        self.font_rectangle.x += 10
 
     def render(self, screen: Surface):
-        self.listen_for_text()
-        self.update_text(screen)
+        self.listen_for_focus()
+
+        screen.blit(self.font_surface, self.font_rectangle)
+        pg.draw.rect(screen, self.input_color, self.input_rectangle, 2)
