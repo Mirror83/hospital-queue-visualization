c294ae10a968a8a2d7917c797c070929ea341742
Mirror83 <glenochieng045@gmail.com>
2023-11-10 09:20:48 +0300

    Added callback functions for each button with output to the terminal

--- main.py
+++ main.py
@@ -10,46 +10,109 @@ from text_button import TextButton
 from text_input import TextInput
 
 
+def update_key_locator_dict():
+    key_locator_dict.clear()
+    for locator in pq.locators():
+        key_locator_dict[locator.key] = locator
+
+
+def print_updates(tag: str = ""):
+    if tag:
+        print(f"{tag}: {pq}")
+        print(f"{tag}: sequential - {pq.string_sequential()}")
+        print(f"{tag}: {key_locator_dict}")
+    else:
+        print(pq)
+        print("sequential - " + pq.string_sequential())
+        print(key_locator_dict)
+
+
 def on_add():
     try:
         key = int(text_inputs[0].text)
         value = text_inputs[1].text
-        index_locator_dict[key] = pq.add(key, value)
-        print(pq)
-        print(index_locator_dict)
+        key_locator_dict[key] = pq.add(key, value)
+        print_updates(tag="on_add")
+
+        text_inputs[0].clear()
+        text_inputs[1].clear()
 
     except ValueError:
         if messagebox.showerror(
                 "Invalid input",
-                "Only pass in numeric values for key") == "ok":
+                "Key should be an integer") == "ok":
             text_inputs[0].clear()
 
 
 def on_update():
     try:
-        old_key = int(text_inputs[2].text)
-        new_key = int(text_inputs[3].text)
-        pq.update(index_locator_dict[old_key], new_key, index_locator_dict[old_key].value)
-        # TODO: Update index_locator_map
+        if not pq.is_empty():
+            old_key = int(text_inputs[2].text)
+            new_key = int(text_inputs[3].text)
+            pq.update(key_locator_dict[old_key], new_key, key_locator_dict[old_key].value)
+            update_key_locator_dict()
+            print_updates(tag="on_update")
 
-        print(pq)
-        print(index_locator_dict)
+            text_inputs[2].clear()
+            text_inputs[3].clear()
+        else:
+            print(f"change_priority: The queue is empty!")
 
     except ValueError:
-        if messagebox.showerror("Invalid input", "Only pass in numeric values for key") == "ok":
-            text_inputs[0].clear()
+        if messagebox.showerror("Invalid input", "Key should be an integer") == "ok":
+            text_inputs[2].clear()
+            text_inputs[3].clear()
+
+    except KeyError:
+        if messagebox.showerror("Invalid key", "There is no member with the key given") == "ok":
+            text_inputs[2].clear()
+            text_inputs[3].clear()
 
 
-def insert(key, value):
-    index_locator_dict[key] = pq.insert(key, value)
-
-
-def remove(key):
+def on_remove():
     try:
-        pq.remove(index_locator_dict[key])
-        index_locator_dict.pop(key)
+        if not pq.is_empty():
+            key = int(text_inputs[4].text)
+            pq.remove(key_locator_dict[key])
+            update_key_locator_dict()
+            print_updates(tag="on_remove")
+
+            text_inputs[4].clear()
+        else:
+            print("on_remove: There is nothing to remove!")
+
     except ValueError:
-        print(f"The object with the key {key} does not exist")
+        if messagebox.showerror("Invalid input", "Key should be an integer") == "ok":
+            text_inputs[4].clear()
+
+    except KeyError:
+        if messagebox.showerror("Invalid key", "There is no member with the key given") == "ok":
+            text_inputs[2].clear()
+            text_inputs[3].clear()
+
+
+def on_remove_min():
+    if not pq.is_empty():
+        pq.remove_min()
+        update_key_locator_dict()
+        print_updates(tag="on_remove_min")
+    else:
+        print("There is nothing to remove!")
+
+
+def on_min():
+    if not pq.is_empty():
+        print(f"min={key_locator_dict[pq.min()[0]]}")
+    else:
+        print("min: The queue is empty!")
+
+
+def on_len():
+    print(f"len={len(pq)}")
+
+
+def on_is_empty():
+    print(f"is_empty={pq.is_empty()}")
 
 
 pg.init()
@@ -89,7 +152,7 @@ for i in range(len(text_inputs)):
 focused_input: TextInput | None = None
 exists_focused_input = False
 
-index_locator_dict: dict[int, AdaptablePriorityQueue.Locator] = dict()
+key_locator_dict: dict[int, AdaptablePriorityQueue.Locator] = dict()
 pq = AdaptablePriorityQueue()
 
 text_buttons = [
@@ -103,35 +166,42 @@ text_buttons = [
     TextButton(
         text="Change priority",
         top_left=Vector2(250, 450),
-        on_click_handler=on_add,
+        on_click_handler=on_update,
         font_size=30,
         color="Black"
     ),
     TextButton(
         text="Remove",
         top_left=Vector2(250, 500),
-        on_click_handler=on_add,
+        on_click_handler=on_remove,
         font_size=30,
         color="Black"
     ),
     TextButton(
         text="Remove min",
-        top_left=Vector2(470, 400),
-        on_click_handler=on_add,
+        top_left=Vector2(450, 400),
+        on_click_handler=on_remove_min,
         font_size=30,
         color="Black"
     ),
     TextButton(
         text="Min",
-        top_left=Vector2(470, 450),
-        on_click_handler=on_add,
+        top_left=Vector2(450, 450),
+        on_click_handler=on_min,
         font_size=30,
         color="Black"
     ),
     TextButton(
         text="len",
-        top_left=Vector2(470, 500),
-        on_click_handler=on_add,
+        top_left=Vector2(450, 500),
+        on_click_handler=on_len,
+        font_size=30,
+        color="Black"
+    ),
+    TextButton(
+        text="is_empty",
+        top_left=Vector2(620, 400),
+        on_click_handler=on_is_empty,
         font_size=30,
         color="Black"
     )
